af33bea28496e74b3aef3f5809d2eb6998f55c8e
Shuu <ghwls328@naver.com>
2023-10-10 20:29:29 +0900

    Combine Object

--- Assets/Scripts/DropObject.cs
+++ Assets/Scripts/DropObject.cs
@@ -5,9 +5,39 @@ using UnityEngine;
 public class DropObject : MonoBehaviour
 {
     [SerializeField] private int level;
+    [SerializeField] private CircleCollider2D trigger;
 
     private void Start()
     {
         gameObject.name = level.ToString();
+        tag = "Drop Object";
+    }
+
+    public void Upgrade()
+    {
+        level++;
+        gameObject.name = level.ToString();
+        transform.localScale *= 1.2f;
+        StartCoroutine(Upgrading());
+    }
+
+    // TriggerEnter를 작동시키기 위해 1프레임을 사이에 두고 trigger를 오프, 온 함.
+    private IEnumerator Upgrading()
+    {
+        trigger.enabled = false;
+        yield return null;
+        trigger.enabled = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Drop Object")
+        {
+            if (collision.gameObject.name == gameObject.name)
+            {
+                Upgrade();
+                Destroy(collision.gameObject);
+            }
+        }
     }
 }
